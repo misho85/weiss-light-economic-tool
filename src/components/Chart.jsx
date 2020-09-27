78f2162a3a861be217adce9481306a39ab3b7ee2
M150 <stevovic@gmail.com>
2020-09-27 10:29:17 +0200

    fix mobile chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -25,7 +25,7 @@ const MyResponsiveBar = ({ data }) => {
       axisBottom={{
         tickSize: 5,
         tickPadding: 5,
-        tickRotation: 0,
+        tickRotation: phone ? -45 : 0,
         legend: '',
         legendPosition: 'middle',
         legendOffset: 32,
